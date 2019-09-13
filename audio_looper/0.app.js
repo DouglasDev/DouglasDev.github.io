c01449bf7477abc495e5726e95d344c6187c1e02
DouglasDev <doug012310@yahoo.com>
2019-09-13 17:52:30 -0400

    audio

---
+++ audio_looper/0.app.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/sounds/high.wav":
+/*!*****************************!*\
+  !*** ./src/sounds/high.wav ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"media/high.1bfbd8c.wav\";\n\n//# sourceURL=webpack:///./src/sounds/high.wav?");
+
+/***/ })
+
+}]);
---
+++ audio_looper/1.app.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./src/sounds/low.wav":
+/*!****************************!*\
+  !*** ./src/sounds/low.wav ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"media/low.03c97be.wav\";\n\n//# sourceURL=webpack:///./src/sounds/low.wav?");
+
+/***/ })
+
+}]);
