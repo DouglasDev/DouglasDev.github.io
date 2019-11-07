812d7e7537acddd84622fadd005fab8edcfddfa5
douglasdev <doug012310@yahoo.com>
2019-11-07 14:09:34 -0500

    calc

---
+++ calc/precache-manifest.e70edec559fa5c5fc010f61547900308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9341d3961456d1cbdcec1f337579623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd510fe3267577a9635",
+    "url": "/static/css/main.cb18f186.chunk.css"
+  },
+  {
+    "revision": "59d878b0276e8fe96420",
+    "url": "/static/js/2.f6b9ba6d.chunk.js"
+  },
+  {
+    "revision": "6dd510fe3267577a9635",
+    "url": "/static/js/main.37025fc1.chunk.js"
+  },
+  {
+    "revision": "084513feb0db4349f8d5",
+    "url": "/static/js/runtime-main.dea7951c.js"
+  }
+]);
